64490db3af6682d1639fd2b4b38e9c835fe82b99
Tony <tonyhuang0446@gmail.com>
2018-06-15 23:40:10 -0700

    Update company tags

---
+++ src/Tag/company/Bloomberg.java
@@ -0,0 +1,16 @@
+package Tag.company;
+
+import Tag.Tag;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+@Target({TYPE, METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("Bloomberg")
+public @interface Bloomberg {
+}
