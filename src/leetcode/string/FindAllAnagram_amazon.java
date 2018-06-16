e8583c99a9ee2ff9672f98e411c0db91f6875aa5
Tony <tonyhuang0446@gmail.com>
2018-06-15 23:41:31 -0700

    Refactor with annotation

--- src/leetcode/string/FindAllAnagram_amazon.java
+++ src/leetcode/string/FindAllAnagram_amazon.java
@@ -1,11 +1,48 @@
 package leetcode.string;
 
+import Tag.company.Amazon;
+import Tag.type.StringModify;
+
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+
+ Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
+
+ The order of output does not matter.
+
+ Example 1:
+
+ Input:
+ s: "cbaebabacd" p: "abc"
+
+ Output:
+ [0, 6]
+
+ Explanation:
+ The substring with start index = 0 is "cba", which is an anagram of "abc".
+ The substring with start index = 6 is "bac", which is an anagram of "abc".
+ Example 2:
+
+ Input:
+ s: "abab" p: "ab"
+
+ Output:
+ [0, 1, 2]
+
+ Explanation:
+ The substring with start index = 0 is "ab", which is an anagram of "ab".
+ The substring with start index = 1 is "ba", which is an anagram of "ab".
+ The substring with start index = 2 is "ab", which is an anagram of "ab".
+
+ */
+
+@Amazon
 public class FindAllAnagram_amazon
 {
-
+    @StringModify
     public static List<Integer> findAnagrams(String s, String p)
     {
         int[] chars = new int[26];
@@ -54,7 +91,7 @@ public class FindAllAnagram_amazon
         // Go over the string
         for (int i = 0; i + p.length() <= s.length(); i++)
         {
-            
+
         }
 
         return result;
--- src/leetcode/string/FisrtUniqueCharacterInString.java
+++ src/leetcode/string/FisrtUniqueCharacterInString.java
@@ -1,16 +1,36 @@
 package leetcode.string;
 
+import Tag.company.Amazon;
+import Tag.company.Bloomberg;
+import Tag.company.Google;
+import Tag.company.Microsoft;
+import Tag.type.StringModify;
+
 /**
- * Given a string, find the first non-repeating character in it and return it's
- * index. If it doesn't exist, return -1.
+ Given a string, find the first non-repeating character in it and return it's
+ index. If it doesn't exist, return -1.
+ Examples:
+
+ s = "leetcode"
+ return 0.
+
+ s = "loveleetcode",
+ return 2.
  */
+
+@Amazon
+@Google
+@Microsoft
+@Bloomberg
 public class FisrtUniqueCharacterInString
 {
-    // use index as to access the frequency
+    @StringModify
     public static int firstUniqChar(String s)
     {
+        // use index as to access the frequency
+        // initial with alphabetic array
         int[] freq = new int[26];
-        
+
         for (int i = 0; i< s.length(); ++i)
         {
             freq[s.charAt(i) - 'a']++;
@@ -25,7 +45,6 @@ public class FisrtUniqueCharacterInString
 
     public static void main(String[] args)
     {
-        
-        
+
     }
 }
