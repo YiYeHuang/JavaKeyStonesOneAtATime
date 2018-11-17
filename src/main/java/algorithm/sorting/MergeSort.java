bb9751b72e5a33dfafadc80971d5101ce3818d1d
Tony <tonyhuang0446@gmail.com>
2018-11-16 23:51:02 -0800

    Merge sort review update

---
+++ src/main/java/algorithm/sorting/MergeSort.java
@@ -0,0 +1,125 @@
+package algorithm.sorting;
+
+public class MergeSort
+{
+
+    /**
+     * Best: O(nlog(n))
+     * Worst: O(nlog(n))
+     * Average: O(nlog(n))
+     * Space: O(nlog(n)) extra space when merge
+     * Stable: yes
+     *
+     *
+     * Like QuickSort, Merge Sort is a Divide and Conquer algorithm.
+     * It divides input array in two halves, calls itself for the two halves and then merges the two sorted halves. The merge()
+     * function is used for merging two halves. The merge(arr, l, m, r) is key process that assumes that
+     * arr[l..m] and arr[m+1..r] are sorted and merges the two sorted sub-arrays into one. See following
+     *
+     *
+     * merge_sort(p...r) = merge(merge_sort(p...q), merge_sort(q+1...r))
+     *
+     * T(n)=Cn+nlog2n
+     */
+    void merge(int arr[], int start, int mid, int end)
+    {
+        // Merges two subarrays of arr[].
+        // First subarray is arr[l..m]
+        // Second subarray is arr[m+1..r]
+
+        // Find sizes of two subarrays to be merged
+        int leftSize = mid - start + 1;
+        int rightSize = end - mid;
+
+        /* Create temp arrays */
+        int L[] = new int[leftSize];
+        int R[] = new int[rightSize];
+
+        /* Copy data to temp arrays */
+        for (int i = 0; i < leftSize; ++i)
+            L[i] = arr[start + i];
+        for (int j = 0; j < rightSize; ++j)
+            R[j] = arr[mid + 1 + j];
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int currentWalker = start;
+        while (i < leftSize && j < rightSize)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[currentWalker] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[currentWalker] = R[j];
+                j++;
+            }
+            currentWalker++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < leftSize)
+        {
+            arr[currentWalker] = L[i];
+            i++;
+            currentWalker++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < rightSize)
+        {
+            arr[currentWalker] = R[j];
+            j++;
+            currentWalker++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void sort(int arr[], int start, int end)
+    {
+        if (start < end)
+        {
+            // Find the middle point
+            int mid = (start + end) / 2;
+
+            // Sort first and second halves
+            sort(arr, start, mid);
+            sort(arr, mid + 1, end);
+
+            // Merge the sorted halves
+            merge(arr, start, mid, end);
+        }
+    }
+
+    /* A utility function to print array of size n */
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String args[])
+    {
+        int arr[] =
+        {12, 11, 13, 5, 6, 7 };
+
+        System.out.println("Given ArrayTag");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
+}
--- src/main/java/algorithm/sorting/README.md
+++ src/main/java/algorithm/sorting/README.md
@@ -21,11 +21,12 @@ way more complicate, if a list of orders are sort by incoming timestamp, it won'
 within the section are processed first. Stability sort comes in handy in this case.  
 
 ## Compare                                     
-| Type                 | Best | Worst   | Average   |
-| -----------------------| :------------- | :------------- |:------------- |
-| Insertion Sort    | O(n)| O(n^2) | O(n^2)|
-| Selection Sort   | O(n^2)| O(n^2) | O(n^2)|
-| Bubble Sort   | O(n)| O(n^2) | O(n^2)|
+| Type                 | Best | Worst   | Average   | Stable   | In Place   |
+| -----------------------| :------------- | :------------- |:------------- | :------------- |:------------- |
+| Insertion Sort    | O(n)| O(n^2) | O(n^2)| Yes | Yes |
+| Selection Sort   | O(n^2)| O(n^2) | O(n^2)| No | Yes |
+| Bubble Sort   | O(n)| O(n^2) | O(n^2)| Yes | Yes |
+| Merge Sort   | O(nlog(n))| O(nlog(n)) | O(nlog(n))| Yes | no O(nlog(n)) |
 | Quick Sort   | |
 | Bucket Sort   | |
 | Counting Sort   | |
