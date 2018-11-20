601cf6d2d66f98be3938ba3655b63dbf990bc350
YiYeHuang <tonyhuang0446@gmail.com>
2018-11-19 21:51:04 -0800

    more functions on ListNode to get better debug experience

--- src/main/java/baseObj/ListNode.java
+++ src/main/java/baseObj/ListNode.java
@@ -5,4 +5,25 @@ public class ListNode
     public int val;
     public ListNode next;
     public ListNode(int x) { val = x; }
+
+    public ListNode add(int value) {
+        ListNode newNode = new ListNode(value);
+        this.next = newNode;
+
+        return newNode;
+    }
+
+    @Override
+    public String toString() {
+        ListNode head = this;
+        StringBuilder sb = new StringBuilder();
+        while (head != null) {
+            sb.append(head.val).append("->");
+            head = head.next;
+        }
+        sb.append("null");
+
+        return sb.toString();
+    }
+
 }
