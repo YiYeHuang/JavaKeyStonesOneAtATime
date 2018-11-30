05ce765cea3c1537c604a19d50e9bacc2290e6a4
Tony <tonyhuang0446@gmail.com>
2018-11-29 23:53:36 -0800

    230. Kth Smallest Element in a BST

---
+++ src/main/java/leetcode/tree/KthSmallestElementinBST.java
@@ -0,0 +1,129 @@
+package leetcode.tree;
+
+import baseObj.TreeNode;
+import leetcode.tag.company.Amazon;
+import leetcode.tag.company.Facebook;
+import leetcode.tag.company.Google;
+import leetcode.tag.company.Microsoft;
+import leetcode.tag.level.Easy;
+import leetcode.tag.level.Medium;
+import leetcode.tag.type.BinarySearch;
+import leetcode.tag.type.Tree;
+
+import java.util.Stack;
+
+/**
+ * Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+ *
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+ *
+ * Example 1:
+ *
+ * Input: root = [3,1,4,null,2], k = 1
+ *    3
+ *   / \
+ *  1   4
+ *   \
+ *    2
+ * Output: 1
+ * Example 2:
+ *
+ * Input: root = [5,3,6,2,4,null,null,1], k = 3
+ *        5
+ *       / \
+ *      3   6
+ *     / \
+ *    2   4
+ *   /
+ *  1
+ * Output: 3
+ * Follow up:
+ * What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+ */
+
+@Facebook
+@Amazon
+@Microsoft
+@Google
+
+@Medium
+@BinarySearch
+@Tree
+public class KthSmallestElementinBST {
+
+    /**
+     * Typical DFS traversal template with stack
+     *
+     * Extra memory usage
+     */
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> smallestCount = new Stack<>();
+
+        // go all the way left
+        while (root != null) {
+            smallestCount.push(root);
+            root = root.left;
+        }
+
+        while (k != 0) {
+            TreeNode next = smallestCount.pop();
+            k--;
+            if (k== 0) {
+                return next.value;
+            }
+
+            TreeNode right = next.right;
+            while (right != null) {
+                smallestCount.push(right);
+                root = root.left;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * ===============================================================================
+     */
+    public int kthSmallestRecur(TreeNode root, int k) {
+        int count = countNodes(root.left);
+
+        if (k < count) {
+            return kthSmallest(root.left, k);
+        } else if (k > count + 1) {
+            return kthSmallest(root.right, k -1 - count);// 1 is counted as current node
+        }
+
+        return root.value;
+    }
+
+    public int countNodes(TreeNode n) {
+        if (n == null) return 0;
+
+        return 1 + countNodes(n.left) + countNodes(n.right);
+    }
+
+    /**
+     * ===============================================================================
+     */
+
+    private static int number = 0;
+    private static int count = 0;
+
+    public int kthSmallestPUREInOrder(TreeNode root, int k) {
+        count = k;
+        helper(root);
+        return number;
+    }
+
+    public void helper(TreeNode n) {
+        if (n.left != null) helper(n.left);
+        count--;
+        if (count == 0) {
+            number = n.value;
+            return;
+        }
+        if (n.right != null) helper(n.right);
+    }
+}
