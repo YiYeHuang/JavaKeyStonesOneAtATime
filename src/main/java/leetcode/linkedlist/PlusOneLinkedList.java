3ba7f4c47897aa39827bd292ca440680f1cabcb0
Tony <tonyhuang0446@gmail.com>
2018-12-04 01:23:15 -0800

    92. Reverse Linked List II

---
+++ src/main/java/leetcode/linkedlist/PlusOneLinkedList.java
@@ -0,0 +1,77 @@
+package leetcode.linkedlist;
+
+import baseObj.ListNode;
+import concurrency.list.List;
+import leetcode.tag.company.Google;
+import leetcode.tag.level.Medium;
+import leetcode.tag.type.LinkedListTag;
+
+import java.util.Stack;
+
+/**
+ * Given a non-negative integer represented as non-empty a singly linked list of digits, plus one to the integer.
+ *
+ * You may assume the integer do not contain any leading zero, except the number 0 itself.
+ *
+ * The digits are stored such that the most significant digit is at the head of the list.
+ *
+ * Example :
+ *
+ * Input: [1,2,3]
+ * Output: [1,2,4]
+ */
+
+@Google
+
+@Medium
+@LinkedListTag
+public class PlusOneLinkedList {
+
+    /**
+     * Key: only 9 will cause overflow, value is only 1
+     *
+     * if last digit is less than 9, no worry
+     * if last digit is 9 and, previous is not, no worry
+     *
+     * only 99999 will cause issue
+     *
+     * point to the last non 9 digit to ZeroFied the rest,
+     * dummy head will take care of the carryover
+     */
+    public static ListNode plusOne(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode lastNotNine = dummy;
+        ListNode walker = dummy;
+
+        while (walker.next != null) {
+            walker = walker.next;
+            if (walker.val != 9) {
+                lastNotNine = walker;
+            }
+        }
+
+        if (walker.val != 9) {
+            walker.val++;
+        } else {
+            lastNotNine.val++;
+            lastNotNine = lastNotNine.next;
+            while (lastNotNine != null) {
+                lastNotNine.val = 0;
+                lastNotNine = lastNotNine.next;
+            }
+        }
+
+        if (dummy.val == 0) {
+            return dummy.next;
+        }
+
+        return dummy;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(9);
+        head.add(9).add(9).add(9);
+        System.out.println(plusOne(head));
+    }
+}
---
+++ src/main/java/leetcode/linkedlist/ReverseLinkedListII.java
@@ -0,0 +1,63 @@
+package leetcode.linkedlist;
+
+import baseObj.ListNode;
+
+/**
+ * Reverse a linked list from position m to n. Do it in one-pass.
+ *
+ * Note: 1 ≤ m ≤ n ≤ length of list.
+ *
+ * Example:
+ *
+ * Input: 1->2->3->4->5->NULL, m = 2, n = 4
+ * Output: 1->4->3->2->5->NULL
+ */
+public class ReverseLinkedListII {
+
+    public static ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || head.next == null ) return head;
+
+        ListNode dummy = new ListNode(0); // create a dummy node to mark the head of this list
+        dummy.next = head;
+
+        // find the head before reverse
+        ListNode pre = dummy;
+        int step = m;
+        while (step > 1) {
+            pre = pre.next;
+            step--;
+        }
+
+        // start as the reverse head, will be tail
+        ListNode tailTobe = pre.next;
+        ListNode reverseNext = tailTobe.next;
+
+        step = n - m;
+
+        // move next
+        // break previous connection
+
+        while (step > 0) {
+            // move to next item
+            tailTobe.next = reverseNext.next;
+
+            // point the current next to previous head
+            reverseNext.next = pre.next;
+
+            // move up pre
+            pre.next = reverseNext;
+
+            // update the next to move
+            reverseNext = tailTobe.next;
+            step--;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.add(2).add(3).add(4).add(5);
+        System.out.println(reverseBetween(head, 2, 4));
+    }
+}
