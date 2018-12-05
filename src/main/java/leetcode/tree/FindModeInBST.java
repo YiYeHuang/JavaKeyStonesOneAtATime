01a9ffcbefbbc7b7d0b0a45048eeaee747bd0172
YiYeHuang <tonyhuang0446@gmail.com>
2018-12-04 17:51:57 -0800

    501. Find Mode in Binary Search Tree

---
+++ src/main/java/leetcode/tree/FindModeInBST.java
@@ -0,0 +1,92 @@
+package leetcode.tree;
+
+import algorithm.sorting.BSTUtil;
+import baseObj.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Given a binary search tree (BST) with duplicates, find all the mode(s) (the most frequently occurred element) in the given BST.
+
+ Assume a BST is defined as follows:
+
+ The left subtree of a node contains only nodes with keys less than or equal to the node's key.
+ The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
+ Both the left and right subtrees must also be binary search trees.
+
+
+ For example:
+ Given BST [1,null,2,2],
+
+  1
+   \
+   2
+ /
+ 2
+
+
+ return [2].
+
+ Note: If a tree has more than one mode, you can return them in any order.
+
+ Follow up: Could you do that without using any extra space? (Assume that the implicit stack space incurred due to recursion does not count).
+ */
+public class FindModeInBST {
+
+	public static int max = 0;
+
+	public static int[] findMode(TreeNode root) {
+		if (root == null) return new int[0];
+
+		Map<Integer, Integer> result = new HashMap<>();
+		TreeNode walker = root;
+		load(walker, result);
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		Iterator<Map.Entry<Integer, Integer>> itr = result.entrySet().iterator();
+		while(itr.hasNext()) {
+			Map.Entry<Integer, Integer> next = itr.next();
+			if (max >= 1 && next.getValue() == max) {
+				list.add(next.getKey());
+			}
+		}
+
+		int[] res = new int[list.size()];
+		int counter = 0;
+		for (Integer item:list) {
+			res[counter]=item;
+			counter++;
+		}
+		return res;
+	}
+
+	public  static void load(TreeNode root, Map<Integer, Integer> result) {
+		if (null == root) return;
+
+		load(root.left, result);
+		if (result.containsKey(root.value)) {
+			result.put(root.value, 1 + result.get(root.value));
+			if (result.get(root.value) >= max) {
+				max = result.get(root.value);
+			}
+		}
+		else {
+			result.put(root.value, 1);
+			if (1 > max) {
+				max=1;
+			}
+		}
+
+		load(root.right, result);
+	}
+
+	public static void main(String[] args) {
+		int[] input = {0,6,2,8,0,4,7,9,-1,-1,2,6};
+		TreeNode root = BSTUtil.toPositiveBST(input);
+		findMode(root);
+	}
+}
