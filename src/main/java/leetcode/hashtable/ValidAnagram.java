74ce2c57a81c21a33449b7cdbdcad4d83031091e
YiYeHuang <tonyhuang0446@gmail.com>
2018-12-06 21:33:59 -0800

    242. Valid Anagram

---
+++ src/main/java/leetcode/hashtable/ValidAnagram.java
@@ -0,0 +1,67 @@
+package leetcode.hashtable;
+
+import leetcode.tag.company.Amazon;
+import leetcode.tag.company.Bloomberg;
+import leetcode.tag.company.Facebook;
+import leetcode.tag.company.Google;
+import leetcode.tag.company.Microsoft;
+import leetcode.tag.level.Easy;
+import leetcode.tag.type.Hash;
+
+/**
+ 242. Valid Anagram
+ Easy
+ 524
+ 87
+
+
+ Given two strings s and t , write a function to determine if t is an anagram of s.
+
+ Example 1:
+
+ Input: s = "anagram", t = "nagaram"
+ Output: true
+ Example 2:
+
+ Input: s = "rat", t = "car"
+ Output: false
+ Note:
+ You may assume the string contains only lowercase alphabets.
+
+ Follow up:
+ What if the inputs contain unicode characters? How would you adapt your solution to such case?
+ */
+
+@Bloomberg
+@Google
+@Amazon
+@Microsoft
+@Facebook
+
+@Easy
+@Hash
+public class ValidAnagram {
+
+	/**
+	 * Typical int[26] hash
+	 */
+	public boolean isAnagram(String s, String t) {
+		int[] range = new int[26];
+
+		for (int i =0; i < s.length(); i++) {
+			range[s.charAt(i) - 'a']++;
+		}
+
+		for (int i =0; i < t.length(); i++) {
+			range[t.charAt(i) - 'a']--;
+		}
+
+		for (int i =0; i < range.length; i++) {
+			if (range[i] != 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
