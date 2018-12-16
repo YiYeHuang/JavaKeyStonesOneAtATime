3666b4f12507a7958189bc864070269054b8ad87
Tony <tonyhuang0446@gmail.com>
2018-12-15 23:57:28 -0800

    MaxDepth bfs dfs update

--- src/main/java/leetcode/tree/MaxDepth.java
+++ src/main/java/leetcode/tree/MaxDepth.java
@@ -4,8 +4,13 @@ import baseObj.TreeNode;
 import leetcode.tag.company.Google;
 import leetcode.tag.company.LinkedIn;
 import leetcode.tag.level.Easy;
+import leetcode.tag.type.BFS;
 import leetcode.tag.type.DFS;
 import leetcode.tag.type.Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  Given a binary tree, find its maximum depth.
@@ -32,12 +37,15 @@ import leetcode.tag.type.Tree;
 @Easy
 @Tree
 @DFS
+@BFS
 public class MaxDepth
 {
     /**
      * alway understand as find tree height
+     *
+     * Recur version
      */
-    public int maxDepth(TreeNode root)
+    public int maxDepthRecur(TreeNode root)
     {
         if (null == root)
         {
@@ -45,7 +53,74 @@ public class MaxDepth
         }
         else
         {
-            return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+            return 1 + Math.max(maxDepthRecur(root.left), maxDepthRecur(root.right));
         }
     }
+
+    /**
+     * DFS, use stack two stack to track node and height.
+     *
+     * moving down with keep popping stack, same level saves same height value
+     */
+    public int maxDepthDFS(TreeNode root) {
+        if (root == null) return 0;
+
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> value = new Stack<>();
+
+        // level 1
+        stack.push(root);
+        value.push(1);
+        int maxHeight = 0;
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            int tempHight = value.pop();
+
+            maxHeight = Math.max(tempHight, maxHeight);
+
+            if (node.left != null) {
+                stack.push(node.left);
+                value.push(tempHight + 1);
+            }
+
+            if (node.right != null) {
+                stack.push(node.right);
+                value.push(tempHight + 1);
+            }
+        }
+
+        return maxHeight;
+    }
+
+    /**
+     * BFS
+     */
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            // consume all the current level node, which means one level is passed, level++
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                size --;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            level++;
+        }
+
+        return level;
+    }
+
 }
