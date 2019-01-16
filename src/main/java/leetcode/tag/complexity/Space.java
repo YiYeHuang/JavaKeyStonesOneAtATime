b9f61c2df5662d8da77bbf130c34a7565a07345e
Tony <tonyhuang0446@gmail.com>
2019-01-16 00:41:24 -0800

    complexity tag

---
+++ src/main/java/leetcode/tag/complexity/Space.java
@@ -0,0 +1,14 @@
+package leetcode.tag.complexity;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+@Target({TYPE, METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Space {
+    String value();
+}
