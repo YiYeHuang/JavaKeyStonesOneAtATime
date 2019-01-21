2ddb4bed47e3671b493eb67e38d943d2ac870471
YiYeHuang <tonyhuang0446@gmail.com>
2019-01-21 00:20:28 -0800

    198. House Robber

--- src/main/java/leetcode/dp/HouseRobber.java
+++ src/main/java/leetcode/dp/HouseRobber.java
@@ -1,76 +1,61 @@
 package leetcode.dp;
 
 /**
- * You are a professional robber planning to rob houses along a street. Each
- * house has a certain amount of money stashed, the only constraint stopping you
- * from robbing each of them is that adjacent houses have security system
- * connected and it will automatically contact the police if two adjacent houses
- * were broken into on the same night.
- * 
- * Given a list of non-negative integers representing the amount of money of
- * each house, determine the maximum amount of money you can rob tonight without
- * alerting the police.
+ 198. House Robber
+
+ You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+ Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+ Example 1:
+
+ Input: [1,2,3,1]
+ Output: 4
+ Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+ Total amount you can rob = 1 + 3 = 4.
+ Example 2:
+
+ Input: [2,7,9,3,1]
+ Output: 12
+ Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+ Total amount you can rob = 2 + 9 + 1 = 12.
+
  */
 public class HouseRobber
 {
     /**
-     * Let us denote that:
-     * 
-     * f(k) = Largest amount that you can rob from the first k houses. 
-     * Ai = Amount of money at the ith house. 
-
-     * n = 1,
-     * f(1) = A1.
-     * 
-     * n = 2,
-     *  f(2) = max(A1, A2).
-     * 
-     * For n = 3,
-     * 
-     * Rob the third house, and add its amount to the first house's amount.
-     * 
-     * Do not rob the third house, and stick with the maximum amount of the
-     * first two houses.
-     * 
-     * f(k) = max(f(k – 2) + Ak, f(k – 1)) 
-     * 
-     * We choose the base case as f(–1) =
-     * f(0) = 0, which will greatly simplify our code as you can see.
-     * 
-     * The answer will be calculated as f(n). We could use an array to store and
-     * calculate the result, but since at each step you only need the previous
-     * two maximum values, two variables are suffice.
-     * 
-     * 
-     * 
-     * A1 -> x
-     * A2 -> max(A1, x)
-     * 
-     * 3 -> A1 + x
-     *      Item2
-     * 
-     * 4 -> A1 + A3
-     *      A2 + x
-     *      A1 + x
-     * 
+     * [1,2,3,1]
+     *
+     * n = 0,      get 1
+     * n = 1,      get 2, since 2 > 1
+     *
+     * n = 2       get 4, since 1 + 3 > 2           compare i - 1 and
+     * n = 3       get 4, since 1 + 2 < 1 + 3       compare previous result and
+     *
+     *
+     *
+     *
      * f(k) = max(f(k – 2) + Ak, f(k – 1)) 
      *      
      */
     public static int rob(int[] num)
     {
-        int prevMax = 0;
-        int currMax = 0;
-        for (int x : num) {
-            int temp = currMax;
-            currMax = Math.max(prevMax + x, currMax);
-            prevMax = temp;
+        if (num.length == 0) return 0;
+
+        int[] result = new int[num.length + 1];
+
+        result[0] = 0;
+        result[1] = num[0];
+        for (int i = 1; i < num.length; i++) {
+            int val = num[i];
+            result[i+1] = Math.max(result[i], result[i-1] + val);
         }
-        return currMax;
+        return result[num.length];
     }
 
     public static void main(String[] args)
     {
-        int[] test = {1, 2, 3, 1000};
+        int[] test = {1, 2, 3, 1};
         System.out.println(rob(test));
     }
 }
