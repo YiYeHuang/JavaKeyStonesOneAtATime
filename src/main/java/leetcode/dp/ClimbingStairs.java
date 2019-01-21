6cfd24785affe227fef0a3fe4cfe877225492728
YiYeHuang <tonyhuang0446@gmail.com>
2019-01-21 00:08:39 -0800

    70. Climbing Stairs

--- src/main/java/leetcode/dp/ClimbingStairs.java
+++ src/main/java/leetcode/dp/ClimbingStairs.java
@@ -1,22 +1,61 @@
 package leetcode.dp;
 
+import leetcode.tag.company.Amazon;
+import leetcode.tag.company.Bloomberg;
+import leetcode.tag.company.Google;
+import leetcode.tag.company.Microsoft;
+import leetcode.tag.level.Easy;
+import leetcode.tag.type.DP;
+
 /**
- * You are climbing a stair case. It takes n steps to reach to the top.
- * 
- * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
- * 
- * Note: Given n will be a positive integer.
+ 70. Climbing Stairs
+
+ You are climbing a stair case. It takes n steps to reach to the top.
+
+ Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ Note: Given n will be a positive integer.
+
+ Example 1:
+
+ Input: 2
+ Output: 2
+ Explanation: There are two ways to climb to the top.
+ 1. 1 step + 1 step
+ 2. 2 steps
+ Example 2:
+
+ Input: 3
+ Output: 3
+ Explanation: There are three ways to climb to the top.
+ 1. 1 step + 1 step + 1 step
+ 2. 1 step + 2 steps
+ 3. 2 steps + 1 step
+
  */
+
+@Google
+@Amazon
+@Microsoft
+@Bloomberg
+
+@Easy
+@DP
 public class ClimbingStairs
 {
     /**
      * 0 : 0 
      * 1 : 1
-     * 2 : 1,1   2
-     * 3 : 1,1,1  2,1  1,2
-     * 4 : 1,1,1,1    1,2,1   1,1,2    2,1,1   2,2
+     * 2 : 1,       1 2
+     * 3 : 1,1,1    2,1     1,2
+     * 4 : 1,1,1,1  1,2,1   1,1,2    2,1,1   2,2
      * 
-     * It is Fibonacci
+     * For
+     * Step(n) = Take one step from Step[n - 1] or Take two step from Step[n - 2]
+     *
+     * Step(n) = Step[n - 1] + Step[n - 2]
+     *
+     * So actually it is Fibonacci Sequence
      */
     public static int climbStairs(int n)
     {
