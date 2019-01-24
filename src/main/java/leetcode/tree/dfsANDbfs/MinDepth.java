087d176f1b0041c7ec83b06146f272da49433011
Tony <tonyhuang0446@gmail.com>
2019-01-24 00:38:51 -0800

    Minimum Depth of Binary Tree update

--- src/main/java/leetcode/tree/dfsANDbfs/MinDepth.java
+++ src/main/java/leetcode/tree/dfsANDbfs/MinDepth.java
@@ -24,10 +24,10 @@ import leetcode.tag.type.Tree;
 
  Given binary tree [3,9,20,null,null,15,7],
 
- 3
- / \
- 9  20
- /  \
+        3
+      /    \
+     9     20
+   /  \
  15   7
  return its minimum depth = 2.
  */
