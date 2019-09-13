9269f3a93a6deb3cc764ef0a9880d8e24fa04b42
Tony Huang <thuang@stellar.com>
2019-09-13 16:43:51 -0700

    153. Find Minimum in Rotated Sorted Array *2

--- src/main/java/leetcode/binarySearch/FindMinimumRotatedSortedArray.java
+++ src/main/java/leetcode/binarySearch/FindMinimumRotatedSortedArray.java
@@ -9,7 +9,7 @@ import leetcode.tag.type.BinarySearch;
 
 /**
  153. Find Minimum in Rotated Sorted Array
- 
+
  Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 
  (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
@@ -38,43 +38,34 @@ import leetcode.tag.type.BinarySearch;
 @BinarySearch
 public class FindMinimumRotatedSortedArray {
 
-    /**
-     * since the original list is sorted, if rotated,
-     *
-     * if num[mid] > start and > end, then it is at the bigger part, low to mid + 1.
-     *
-     * if not it is at the smaller part low to mid -1
-     *
-     * if cannot find, rotate at 0, no rotate
-     *
-     *
-     */
     public static int findMin(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        if (nums.length == 1) {
-            return nums[0];
-        }
-        int low = 0;
-        int high = nums.length -1;
+        if (nums == null || nums.length == 0) return 0;
 
-        while (low < high) {
+        int low = 0;
+        int high = nums.length-1;
+
+        // If num[i-1] > num[i] < num[i+1], then num[i] is min
+        // else
+        //  mid < num[low] and mid < num[high], min on the left
+        //  mid  > num[low] and mid > num[high], min on the right
+        while(low < high){
+            // deal with the edge cases:
+            // if(low +1== high)
+            //     return nums[low] > nums[high]? low : high;
+
             int mid = low + (high - low)/2;
 
-            // not mid + 1 to avoid out of bounce
-            if (mid !=0 && nums[mid] < nums[mid - 1]) {
+            // mid is the low
+            if(mid > 0 && nums[mid] < nums[mid-1] && nums[mid] < nums[mid+1]) {
                 return nums[mid];
             }
 
-            // high change that low is gonna tie to mid, do mid >= low
-            if (nums[mid] >= nums[low] && nums[mid] > nums[high]) {
+            if (nums[low] <= nums[mid] && nums[mid] > nums[high]) {
                 low = mid + 1;
             } else {
-                high = mid -1;
+                high = mid - 1;
             }
         }
-
         return nums[low];
     }
 
