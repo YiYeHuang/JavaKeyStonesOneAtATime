c2da8af2304ca90d25ad57c100f06ad3b16a4812
Tony Huang <thuang@stellar.com>
2019-09-13 16:44:50 -0700

    update existing

--- src/main/java/leetcode/binarySearch/FirstBadVersion.java
+++ src/main/java/leetcode/binarySearch/FirstBadVersion.java
@@ -1,5 +1,8 @@
 package leetcode.binarySearch;
 
+import leetcode.tag.level.Easy;
+import leetcode.tag.type.BinarySearch;
+
 /**
  * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
  *
@@ -17,25 +20,34 @@ package leetcode.binarySearch;
  *
  * Then 4 is the first bad version.
  */
+
+@Easy
+@BinarySearch
 public class FirstBadVersion {
 
     /**
      * G G G G B B B B B B
      */
     public int firstBadVersion(int n) {
-        int low = 1;
-        int high = n;
-        int mid=1;
-        while (low < high) {
-            mid = low + (high - low)/2;
 
-            if (isBadVersion(mid))
+        int low = 0, high = n;
+        while(low < high){
+            // Prevent (left + right) overflow
+            int mid = low + (high - low) / 2;
+
+            if(isBadVersion(mid)) {
+                // high = mid -1 might cause skip a version check
                 high = mid;
-            else
+            } else {
+                // low = mid + 1 is fine, mid + 1 might be the fist bad version
                 low = mid + 1;
+            }
         }
 
-        return low;
+        if(!isBadVersion(low))
+            return low + 1;
+        else
+            return low;
     }
 
     public boolean isBadVersion(int input) {
