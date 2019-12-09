7c0eb607f557b3ba9483331485195940782b5ec4
Tony <tonyhuang0446@gmail.com>
2019-12-09 00:04:36 -0800

    687. Longest Univalue Path

---
+++ src/main/java/leetcode/tree/IsSubTree.java
@@ -0,0 +1,65 @@
+package leetcode.tree;
+
+import baseObj.TreeNode;
+import leetcode.tag.level.Easy;
+import leetcode.tag.type.DFS;
+import leetcode.tag.type.Tree;
+
+/*
+687. Longest Univalue Path
+
+Given a binary tree, find the length of the longest path where each node in the path has the same value.
+This path may or may not pass through the root.
+
+The length of path between two nodes is represented by the number of edges between them.
+
+
+
+Example 1:
+
+Input:
+
+              5
+             / \
+            4   5
+           / \   \
+          1   1   5
+Output: 2
+
+
+
+Example 2:
+
+Input:
+
+              1
+             / \
+            4   5
+           / \   \
+          4   4   5
+Output: 2
+ */
+
+@Easy
+@Tree
+@DFS
+public class IsSubTree {
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (isSameTree(s,  t)) return true;
+
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
