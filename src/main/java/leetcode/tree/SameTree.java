1c9c7d07dbf7b2004ca6ade6745515e7ade84daa
Tony <tonyhuang0446@gmail.com>
2019-12-09 00:05:20 -0800

    update code base

--- src/main/java/leetcode/tree/SameTree.java
+++ src/main/java/leetcode/tree/SameTree.java
@@ -54,10 +54,7 @@ public class SameTree
         if (p == null || q == null) {
             return false;
         }
-        if (p.val == q.val) {
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        } else {
-            return false;
-        }
+
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
