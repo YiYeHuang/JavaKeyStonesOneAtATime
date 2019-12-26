6c5f1eaec06adf1c21c39e217a7ff2e9d932431b
Tony <tonyhuang0446@gmail.com>
2019-12-25 17:38:33 -0800

    804. Unique Morse Code Words

---
+++ src/main/java/leetcode/string/UniqueMorseCodeWords.java
@@ -0,0 +1,59 @@
+package leetcode.string;
+
+import leetcode.tag.level.Easy;
+import leetcode.tag.type.HashTableTag;
+import leetcode.tag.type.StringTag;
+
+import java.util.HashSet;
+import java.util.Set;
+/*
+804. Unique Morse Code Words
+
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes,
+as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter.
+For example, "cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-").
+We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation:
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+There are 2 different transformations, "--...-." and "--...--.".
+ */
+
+
+@Easy
+@StringTag
+@HashTableTag
+public class UniqueMorseCodeWords {
+
+    String[] dict = {".-","-...","-.-.","-..",".","..-.","--.","....","..",
+            ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+    public int uniqueMorseRepresentations(String[] words) {
+        Set<String> lookUp = new HashSet<>();
+
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                sb.append(dict[word.toLowerCase().charAt(i) - 'a']);
+            }
+            lookUp.add(sb.toString());
+        }
+
+        return lookUp.size();
+    }
+}
