6c6552b72b627ffe02a207cdefa2c34d0c611054
Tony <tonyhuang0446@gmail.com>
2020-04-06 00:11:56 -0700

    Update RectangleAreaII.java

--- src/main/java/leetcode/lineSweep/RectangleAreaII.java
+++ src/main/java/leetcode/lineSweep/RectangleAreaII.java
@@ -11,15 +11,7 @@ import leetcode.tag.type.LineSweep;
 
 /*
 850. Rectangle Area II
-Hard
 
-277
-
-27
-
-Add to List
-
-Share
 We are given a list of (axis-aligned) rectangles.  Each rectangle[i] = [x1, y1, x2, y2] ,
 where (x1, y1) are the coordinates of the bottom-left corner,
 and (x2, y2) are the coordinates of the top-right corner of the ith rectangle.
@@ -52,74 +44,74 @@ The total area covered by all rectangles will never exceed 2^63 - 1 and thus wil
 @Heap
 public class RectangleAreaII {
 
-  class Node {
-    int x, y, val;
-    Node(int x, int y, int val) {
-      this.x = x;
-      this.y = y;
-      this.val = val;
-    }
-
-    public String toString() {
-      return x + " " + y + " " + val;
-    }
-  }
-
-  public int rectangleArea(int[][] rectangles) {
-    int M = 1000000007;
-    List<Node> data = new ArrayList<>();
-
-    for (int[] r : rectangles) {
-      data.add(new Node(r[0], r[1], 1)); // x1, y1, bottom left, entry point
-      data.add(new Node(r[0], r[3], -1)); // x1, y2, up left, leaving point
-      data.add(new Node(r[2], r[1], -1)); // x2, y1, bottom right, leaving point
-      data.add(new Node(r[2], r[3], 1));  // x2, y2, up left, entry point
-    }
-
-    Collections.sort(data, (a, b) -> {
-      if (a.x == b.x) {
-        return b.y - a.y;
+    class Node {
+      int x, y, val;
+      Node(int x, int y, int val) {
+        this.x = x;
+        this.y = y;
+        this.val = val;
       }
-      return a.x - b.x;
-    });
 
-    TreeMap<Integer, Integer> map = new TreeMap<>();
-    int preX = -1;
-    int preY = -1;
-    int result = 0;
+      public String toString() {
+        return x + " " + y + " " + val;
+      }
+    }
 
-    for (int i = 0; i < data.size(); i++) {
-      Node p = data.get(i);
-      System.out.println(p);
+    public int rectangleArea(int[][] rectangles) {
+      int M = 1000000007;
+      List<Node> data = new ArrayList<>();
 
-      map.put(p.y, map.getOrDefault(p.y, 0) + p.val);
+      for (int[] r : rectangles) {
+        data.add(new Node(r[0], r[1], 1)); // x1, y1, bottom left, entry point
+        data.add(new Node(r[0], r[3], -1)); // x1, y2, up left, leaving point
+        data.add(new Node(r[2], r[1], -1)); // x2, y1, bottom right, leaving point
+        data.add(new Node(r[2], r[3], 1));  // x2, y2, up left, entry point
+      }
 
-      if (i == data.size() - 1 || data.get(i + 1).x > p.x) {
-        if (preX > -1) {
-          result += ((long)preY * (p.x - preX)) % M;
-          result %= M;
+      Collections.sort(data, (a, b) -> {
+        if (a.x == b.x) {
+          return b.y - a.y;
         }
-        preY = calcY(map);
-        preX = p.x;
-      }
-    }
-    return result;
-  }
+        return a.x - b.x;
+      });
 
-  private int calcY(TreeMap<Integer, Integer> map) {
-    int result = 0;
-    int previousCutPoint = -1;
-    int count = 0;
+      TreeMap<Integer, Integer> map = new TreeMap<>();
+      int preX = -1;
+      int preY = -1;
+      int result = 0;
 
-    for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-      if (previousCutPoint >= 0 && count > 0) {
-        result += e.getKey() - previousCutPoint;
+      for (int i = 0; i < data.size(); i++) {
+        Node p = data.get(i);
+        System.out.println(p);
+
+        map.put(p.y, map.getOrDefault(p.y, 0) + p.val);
+
+        if (i == data.size() - 1 || data.get(i + 1).x > p.x) {
+          if (preX > -1) {
+            result += ((long)preY * (p.x - preX)) % M;
+            result %= M;
+          }
+          preY = getY(map);
+          preX = p.x;
+        }
       }
-      count += e.getValue();
-      previousCutPoint = e.getKey();
+      return result;
     }
-    return result;
-  }
+
+    private int getY(TreeMap<Integer, Integer> map) {
+      int result = 0;
+      int previousCutPoint = -1;
+      int count = 0;
+
+      for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+        if (previousCutPoint >= 0 && count > 0) {
+          result += e.getKey() - previousCutPoint;
+        }
+        count += e.getValue();
+        previousCutPoint = e.getKey();
+      }
+      return result;
+    }
 
   public static void main(String[] args) {
     RectangleAreaII rII = new RectangleAreaII();
