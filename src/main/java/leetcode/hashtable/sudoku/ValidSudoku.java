184e6b36e345c160d2896cae7a0b8300d941638c
YiYeHuang <tonyhuang0446@gmail.com>
2020-06-29 15:59:16 -0700

    Create ValidSudoku.java

---
+++ src/main/java/leetcode/hashtable/sudoku/ValidSudoku.java
@@ -0,0 +1,121 @@
+package leetcode.hashtable.sudoku;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 36. Valid Sudoku
+ *
+ * Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ *
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ *
+ * A partially filled sudoku which is valid.
+ *
+ * The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+ *
+ * Example 1:
+ *
+ * Input:
+ * [
+ *   ['5','3','.','.','7','.','.','.','.'],
+ *   ['6','.','.','1','9','5','.','.','.'],
+ *   ['.','9','8','.','.','.','.','6','.'],
+ *   ['8','.','.','.','6','.','.','.','3'],
+ *   ['4','.','.','8','.','3','.','.','1'],
+ *   ['7','.','.','.','2','.','.','.','6'],
+ *   ['.','6','.','.','.','.','2','8','.'],
+ *   ['.','.','.','4','1','9','.','.','5'],
+ *   ['.','.','.','.','8','.','.','7','9']
+ * ]
+ * Output: true
+ * Example 2:
+ *
+ * Input:
+ * [
+ *   ['8','3','.','.','7','.','.','.','.'],
+ *   ['6','.','.','1','9','5','.','.','.'],
+ *   ['.','9','8','.','.','.','.','6','.'],
+ *   ['8','.','.','.','6','.','.','.','3'],
+ *   ['4','.','.','8','.','3','.','.','1'],
+ *   ['7','.','.','.','2','.','.','.','6'],
+ *   ['.','6','.','.','.','.','2','8','.'],
+ *   ['.','.','.','4','1','9','.','.','5'],
+ *   ['.','.','.','.','8','.','.','7','9']
+ * ]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner being
+ *     modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ * Note:
+ *
+ * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ * Only the filled cells need to be validated according to the mentioned rules.
+ * The given board contain only digits 1-9 and the character '.'.
+ * The given board size is always 9x9.
+ */
+public class ValidSudoku {
+
+    public static boolean isValidSudoku(char[][] board) {
+        for(int i = 0; i<9; i++){
+            Set<Character> rows = new HashSet<>();
+            Set<Character> columns = new HashSet<>();
+            Set<Character> cube = new HashSet<>();
+            for (int j = 0; j < 9;j++){
+
+                // check row
+                if(board[i][j]!='.' && !rows.add(board[i][j]))
+                    return false;
+
+                // check column
+                if(board[j][i]!='.' && !columns.add(board[j][i]))
+                    return false;
+
+                // check block
+                int cubeRow = 3*(i/3);
+                int cubeCol = 3*(i%3);
+
+                if  (board[cubeRow + j/3][cubeCol + j%3] !='.' &&
+                    !cube.add(board[cubeRow + j/3][cubeCol + j%3]))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * '4' in row 7 is encoded as "(4)7".
+     * '4' in column 7 is encoded as "7(4)".
+     * '4' in the top-right block is encoded as "0(4)2".
+     */
+    public boolean isValidSudoku_string(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for (int i=0; i<9; ++i) {
+            for (int j=0; j<9; ++j) {
+                if (board[i][j] != '.') {
+                    String b = "(" + board[i][j] + ")";
+                    if (!seen.add(b + i) || !seen.add(j + b) || !seen.add(i/3 + b + j/3))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        char[][] ch = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        isValidSudoku(ch);
+    }
+}
