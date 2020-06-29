e6d3a7bedf3044f4c0d89b09f4254090f1d3919c
YiYeHuang <tonyhuang0446@gmail.com>
2020-06-29 15:59:04 -0700

    Update WordBreak.java

--- src/main/java/leetcode/dp/WordBreak.java
+++ src/main/java/leetcode/dp/WordBreak.java
@@ -1,5 +1,6 @@
 package leetcode.dp;
 
+import java.util.Arrays;
 import leetcode.tag.level.Medium;
 import leetcode.tag.type.DP;
 import leetcode.tag.type.HashTableTag;
@@ -13,11 +14,11 @@ import java.util.Set;
  * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
  * determine if s can be segmented into a space-separated sequence of one or more dictionary words.
  * You may assume the dictionary does not contain duplicate words.
- * 
+ *
  * For example, given s = "leetcode", dict = ["leet", "code"].
- * 
+ *
  * Return true because "leetcode" can be segmented as "leet code".
- * 
+ *
  * UPDATE (2017/1/4): The wordDict parameter had been changed to a list of strings (instead of a set
  * of strings). Please reload the code definition to get the latest changes.
  */
@@ -33,7 +34,7 @@ public class WordBreak
         if (null == s || null == wordDict || wordDict.size() == 0)
             return false;
 
-        Set<String> dict = new HashSet<String>();
+        Set<String> dict = new HashSet<>();
         for (String word : wordDict)
         {
             dict.add(word);
@@ -43,27 +44,19 @@ public class WordBreak
 
         f[0] = true;
 
-        /*
-         * First DP for(int i = 1; i <= s.length(); i++){ for(String str: dict){ if(str.length() <=
-         * i){ if(f[i - str.length()]){ if(s.substring(i-str.length(), i).equals(str)){ f[i] = true;
-         * break; } } } } }
-         */
-
         /**
          * Typical DP question, need an array to store the value
-         * The idea is to store flag to check word_contans end here, 
+         * The idea is to store flag to check word_contans end here,
          */
-        for (int i = 1; i <= s.length(); i++)
-        {
-            for (int j = 0; j < i; j++)
-            {
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
                 String target = s.substring(j, i);
-                System.out.println(target);
-                if (f[j] && dict.contains(target))
-                {
+                if (f[j] && dict.contains(target)) {
+                    System.out.print("Contains " + target + "\n");
                     f[i] = true;
                     break;
                 }
+                System.out.print(target + "\n");
             }
         }
 
@@ -72,11 +65,8 @@ public class WordBreak
 
     public static void main(String[] args)
     {
-        List test = new ArrayList<String>();
-        test.add("leet");
-        test.add("code");
-        test.add("test");
+        List test = Arrays.asList("cats", "dog", "sand", "and", "cat");
 
-        wordBreak("testleetcode", test);
+        wordBreak("catsandog", test);
     }
 }
